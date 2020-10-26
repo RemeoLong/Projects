45d3f5e7fa227b9423152a1b0cff6313e8ec20e3
Long Q Tran <69608736+RemeoLong@users.noreply.github.com>
2020-10-26 16:23:55 -0500

    Add files via upload

---
+++ DiceGame.py
@@ -0,0 +1,23 @@
+import random
+
+dice1 = random.randint(0, 6)
+dice2 = random.randint(0, 6)
+player1_name = ""
+player2_name = ""
+
+player1_name = input("Player 1, please enter your name: ")
+print("Roll the Dice " + player1_name)
+print("Rolling Dice...")
+print(int(dice1))
+
+player2_name = input("Player 2, please enter in your name: ")
+print("Roll the Dice " + player2_name)
+print("Rolling Dice...")
+print(int(dice2))
+
+if dice1 == dice2:
+    print("It was a TIE!")
+elif dice1 < dice2:
+    print(player2_name + ", you win!")
+elif dice1 > dice2:
+    print(player1_name + ", you win!")
